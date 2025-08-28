03d35c20bc36e553926415ec224232f674824b11
mariyo <mario.palka@gmail.com>
2025-08-28 10:36:28 +0200

    **Add Gradle run configuration for Kotlin JS and yarn.lock for dependencies**

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
+plugins {
+    kotlin("multiplatform") version "2.2.10"
+}
+
+group = "eu.mariyo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    js {
+        browser {
+            commonWebpackConfig {
+                cssSupport {
+                    enabled.set(true)
+                }
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).copy(
+                    port = 8010
+                )
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(npm("react", "v19.1.1"))
+            }
+        }
+    }
+}
+
+tasks.withType<KotlinJsCompile>().configureEach {
+    compilerOptions {
+        target = "es2015"
+    }
+}
